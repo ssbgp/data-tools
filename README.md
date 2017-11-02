472a77d39ec64a64557d165124c01d925ec98ef5
David Fialho <fialho.david@protonmail.com>
2017-11-02 11:52:14 +0000

    Initial structure of the project

---
+++ README.md
@@ -0,0 +1,2 @@
+# SS-BGP Data Tools
+Set of tools to process data obtained from the SS-BGP routing simulator
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ssbgp-data-tools',
+    version='0.1',
+    description='Set of tools to process data obtained from the SS-BGP routing '
+                'simulator',
+    url='',
+    license='MIT',
+    author='David Fialho',
+    author_email='fialho.david@protonmail.com',
+
+    packages=find_packages(),
+)
