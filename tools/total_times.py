93ad2f76c186afeada2e2beeece89578f6346374
David Fialho <fialho.david@protonmail.com>
2017-11-06 15:50:54 +0000

    Refactor total times tool

--- tools/total_times.py
+++ tools/total_times.py
@@ -11,24 +11,18 @@ Options:
 
 """
 import csv
-import sys
 from collections import defaultdict
-from pathlib import Path
 
-import numpy as np
-import plotly.offline as py
 from docopt import docopt
-from plotly.graph_objs import Scatter
 
 from processing.application import Application
 from processing.basic_file_selector import BasicFileSelector
 from processing.data_classes import Protocol
 from processing.data_loader import DataLoader
-from processing.data_processor import DataProcessor
-from processing.directory import Directory, EmptyDirectory
 from processing.labeled_file_collection import LabeledFileCollection
 from processing.labeled_file_container import LabeledFileContainer
-from tools.utils import print_error
+from tools.total_times_utils import TotalTimesProcessor
+from tools.utils import get_directory
 
 
 class TotalTimesLoader(DataLoader):
@@ -48,54 +42,6 @@ class TotalTimesLoader(DataLoader):
                     total_times[protocol].append(total_time)
 
         return total_times
-
-
-class TotalTimesProcessor(DataProcessor):
-    """
-    Expects a list of integer values. These integer values should represent the
-    total times of each simulation in the data set.
-    """
-
-    protocol_labels = {
-        Protocol.BGP: "BGP",
-        Protocol.SSBGP: "SSBGP",
-        Protocol.SSBGP2: "SS-BGP2",
-    }
-
-    def process(self, data: dict):
-        scatters = []
-        for protocol, values in data.items():
-            count = len(values)
-            hist, bin_edges = np.histogram(values, bins=range(0, 2001000, 100))
-            cumsum = np.cumsum(hist)
-            cumsum = [(count - value) / count for value in cumsum]
-
-            print(protocol)
-            print("----------------------")
-            print("data:", values)
-            print("bins:", bin_edges)
-            print("hist:", hist)
-            print("cum:", cumsum)
-            print()
-
-            scatters.append(Scatter(x=bin_edges, y=cumsum,
-                                    name=self.protocol_labels[protocol]))
-
-        py.plot(scatters, filename="file.html", auto_open=False)
-
-
-def get_directory(args: dict, key: str):
-    if args[key] is None:
-        return EmptyDirectory()
-
-    directory = Directory(Path(args[key]))
-
-    if directory:
-        if not directory.path.is_dir():
-            print_error("data directory was not found:", directory)
-            sys.exit(1)
-
-    return directory
 
 
 def main():
--- tools/utils.py
+++ tools/utils.py
@@ -1,2 +1,22 @@
+import sys
+from pathlib import Path
+
+from processing.directory import Directory, EmptyDirectory
+
+
 def print_error(*values, sep=' ', end='\n', file=None):
     print("ERROR:", *values, sep=sep, end=end, file=file)
+
+
+def get_directory(args: dict, key: str):
+    if args[key] is None:
+        return EmptyDirectory()
+
+    directory = Directory(Path(args[key]))
+
+    if directory:
+        if not directory.path.is_dir():
+            print_error("data directory was not found:", directory)
+            sys.exit(1)
+
+    return directory
