386af9ea6da6afc809995df80a7915fd01097495
David Fialho <fialho.david@protonmail.com>
2017-11-06 14:43:29 +0000

    Add EmptyDirectory and implement hash, eq, and str

--- processing/directory.py
+++ processing/directory.py
@@ -7,14 +7,40 @@ class Directory(FileContainer):
     """ File container based on a single directory. """
 
     def __init__(self, directory: Path):
-        self.directory = directory
+        self._path = directory
 
     def __iter__(self):
-        return self.directory.iterdir()
+        return self._path.iterdir()
 
     def glob(self, pattern: str):
-        return self.directory.glob(pattern)
+        return self._path.glob(pattern)
+
+    def __eq__(self, other):
+        # noinspection PyProtectedMember
+        return self._path == other._path
+
+    def __hash__(self):
+        return hash(self._path)
 
     @property
     def path(self) -> Path:
-        return self.directory
+        return self._path
+
+    def __str__(self) -> str:
+        return str(self._path)
+
+    def __repr__(self):
+        return repr(self._path)
+
+
+class EmptyDirectory(Directory):
+    """ An empty directory is an abstraction of a directory with no files """
+
+    def __init__(self):
+        super().__init__(Path(""))
+
+    def __iter__(self):
+        return iter([])
+
+    def glob(self, pattern: str):
+        return iter([])
