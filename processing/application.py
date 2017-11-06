a53ee48e8d46e387dcad932f67b55784fab4b99e
David Fialho <fialho.david@protonmail.com>
2017-11-06 15:18:22 +0000

    Update Application to use collections and containers

--- processing/application.py
+++ processing/application.py
@@ -1,18 +1,19 @@
 from processing.data_loader import DataLoader
 from processing.data_processor import DataProcessor
-from processing.file_collection import FileCollection
+from processing.file_container import FileContainer
 from processing.file_selector import FileSelector
 
 
 class Application:
 
-    def __init__(self, selector: FileSelector, loader: DataLoader,
-                 processor: DataProcessor):
+    def __init__(self, container: FileContainer, selector: FileSelector,
+                 loader: DataLoader, processor: DataProcessor):
+        self.container = container
         self.selector = selector
         self.loader = loader
         self.processor = processor
 
-    def run(self, files: FileCollection):
-        data_files = self.selector.select(files)
+    def run(self):
+        data_files = self.selector.select(self.container)
         data = self.loader.load(data_files)
         self.processor.process(data)
