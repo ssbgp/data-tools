6ce1f1f2279b6efd90b534e5c75a3c580ee7d86e
David Fialho <fialho.david@protonmail.com>
2017-12-13 17:07:17 +0000

    Refactor and document plot-times tool

--- tools/plot_times.py
+++ tools/plot_times.py
@@ -1,6 +1,11 @@
 """
 SS-BGP Data Tools: Plot Termination Times
 
+Plots the cumulative sum of the termination times of each destination from multiple sets of data.
+
+The termination time of each destination corresponds to the maximum of all termination times
+among all samples (seeds) of that destination.
+
 Usage:
   plot-times --traces <traces>... [ --out=<output>]
   plot-times --file=<file> [ --out=<output>]
@@ -30,89 +35,6 @@ from processing.labeled_file_collection import LabeledFileCollection
 from processing.labeled_file_container import LabeledFileContainer
 from processing.plotter import Plotter, TraceLine, TraceData
 from tools.utils import print_error
-
-
-class TerminationTimesLoader(DataLoader):
-    """
-    Loads the termination times from each data file in collection *data_files*. Termination times
-    are grouped by label. It includes termination times from simulations that did not terminate.
-    """
-
-    def load(self, data_files: LabeledFileCollection) -> Dict[str, List[int]]:
-        # Container to hold the loaded data
-        termination_times: Dict[str, List[int]] = defaultdict(list)
-
-        for label, data_file in data_files.iter_by_label():
-            # Load termination time from data file
-            max_termination_time = 0
-            with open_csv(data_file) as file:
-                for row in file:
-                    termination_time = int(row["Termination Time (Total)"])
-                    max_termination_time = max(max_termination_time, termination_time)
-
-            termination_times[label].append(max_termination_time)
-
-        return termination_times
-
-
-class Trace(NamedTuple):
-    label: str
-    data_dir: Directory
-    line: TraceLine = {}
-
-
-class TerminationTimesProcessor(DataProcessor):
-    """
-    Expects a list of integer values. These integer values should represent the
-    total times of each simulation in the data set.
-    """
-
-    def __init__(self, plotter: Plotter = None):
-        self._plotter = plotter
-
-    def process(self, data: Dict[str, List[int]]):
-
-        #
-        # Compute the traces for each data input
-        #
-        traces: List[TraceData] = []
-        for label, values in data.items():
-            count = len(values)
-            hist, bin_edges = np.histogram(values, bins=range(0, 2001000, 100))
-            cumulative_sum = np.cumsum(hist)
-            cumulative_sum = [(count - value) / count for value in cumulative_sum]
-
-            traces.append(TraceData(label, x=bin_edges, y=cumulative_sum))
-
-        #
-        # Plot all traces
-        #
-        if self._plotter:
-            self._plotter.plot(traces)
-
-
-def load_traces(path: Path) -> List[Trace]:
-    """ Loads traces from a trace file """
-    with open(path) as file:
-        traces: List[Trace] = []
-        for label, specs in json.load(file).items():
-            trace = Trace(label, Directory(Path(specs['data'])), specs['line'])
-            traces.append(trace)
-
-        return traces
-
-
-def parse_traces(trace_pairs: List[str]) -> List[Trace]:
-    """
-    Takes a list of trace pairs and returns a list of trace instances corresponding to those
-    pairs.
-    """
-    traces: List[Trace] = []
-    for pair in trace_pairs:
-        label, data_dir = pair.split('=', maxsplit=1)
-        traces.append(Trace(label, Directory(Path(data_dir))))
-
-    return traces
 
 
 def main():
@@ -145,6 +67,118 @@ def main():
     )
 
     return app.run()
+
+
+class Trace(NamedTuple):
+    label: str
+    data_dir: Directory
+    line: TraceLine = {}
+
+
+def load_traces(path: Path) -> List[Trace]:
+    """
+    Loads traces from a trace file. The trace file is a JSON file that specifies some
+    configurations for each trace. For example,
+
+        "TRACE LABEL": {
+            "data": "/path/to/data",
+            "line": {
+                "color": "rgb(205, 12, 24)",
+                "dash": "dot"
+            }
+        }
+
+    'TRACE LABEL' corresponds to the label of the trace and must be unique to each trace.
+    Each trace has two main attributes:
+        - data: corresponds to the data directory (its value is '/path/to/data' in the example)
+        - line: specifies a set of attributes to describe how the line is displayed
+
+    A trace file may have multiple traces.
+
+    """
+    with open(path) as file:
+        traces: List[Trace] = []
+        for label, specs in json.load(file).items():
+            trace = Trace(label, Directory(Path(specs['data'])), specs['line'])
+            traces.append(trace)
+
+        return traces
+
+
+def parse_traces(trace_pairs: List[str]) -> List[Trace]:
+    """
+    Takes a list of trace pairs and returns a list of trace instances corresponding to those pairs.
+    A trace pair is composed of a string with the trace's label and data directory separated by
+    an equals sign. For example,
+
+        - Exp1=/path/to/data -> label is 'Exp1' and data directory is '/path/to/data'
+        - Exp1=/path=to/data -> label is 'Exp1' and data directory is '/path=to/data'
+        - Exp1=path/to/data -> label is 'Exp1' and data directory is 'path=to/data'
+        - Ex=p1=/path/to/data -> label is 'Ex' and data directory is 'p1=/path/to/data'
+
+    """
+    traces: List[Trace] = []
+    for pair in trace_pairs:
+        label, data_dir = pair.split('=', maxsplit=1)
+        traces.append(Trace(label, Directory(Path(data_dir))))
+
+    return traces
+
+
+class TerminationTimesLoader(DataLoader):
+    """
+    Loads the termination times from each data file in collection *data_files*. For each data
+    file it considers only the maximum of all termination times among all samples (seeds).
+
+    Termination times are grouped by label.
+    It includes termination times from simulations that did not terminate.
+    """
+
+    def load(self, data_files: LabeledFileCollection) -> Dict[str, List[int]]:
+        # Container to hold the loaded data
+        termination_times: Dict[str, List[int]] = defaultdict(list)
+
+        for label, data_file in data_files.iter_by_label():
+            # Load termination time from data file
+            max_termination_time = 0
+            with open_csv(data_file) as file:
+                for row in file:
+                    termination_time = int(row["Termination Time (Total)"])
+                    max_termination_time = max(max_termination_time, termination_time)
+
+            termination_times[label].append(max_termination_time)
+
+        return termination_times
+
+
+class TerminationTimesProcessor(DataProcessor):
+    """
+    Expects a list of termination times (integer values) for each label.
+    For each label, it computes the cumulative sum and plots it using the specified plotter.
+    """
+
+    def __init__(self, plotter: Plotter = None):
+        self._plotter = plotter
+
+    def process(self, data: Dict[str, List[int]]):
+
+        #
+        # Compute the traces for each data input
+        #
+        traces: List[TraceData] = []
+        for label, values in data.items():
+            count = len(values)
+            hist, bin_edges = np.histogram(values, bins=range(0, 2001000, 100))
+            cumulative_sum = np.cumsum(hist)
+            cumulative_sum = [(count - value) / count for value in cumulative_sum]
+
+            traces.append(TraceData(label, x=bin_edges, y=cumulative_sum))
+
+        #
+        # Plot all traces
+        #
+        if self._plotter:
+            self._plotter.plot(traces)
 
 
 if __name__ == '__main__':
