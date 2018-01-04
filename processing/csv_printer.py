0c9d0b3e7b024995e3507a3e9e751a319c125ed5
David Fialho <fialho.david@protonmail.com>
2018-01-04 14:41:27 +0000

    Add set_headers() method to CSVPrinter
    Setting the headers, not only prevents typos, but also forces the order
    of each column. Previously, the order of the columns was dictated by
    the order of the keys in the dicts.

--- processing/csv_printer.py
+++ processing/csv_printer.py
@@ -1,6 +1,6 @@
 import csv
 from pathlib import Path
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 
 class CSVPrinter:
@@ -17,9 +17,16 @@ class CSVPrinter:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self._file.__exit__(exc_type, exc_val, exc_tb)
 
-    def print_row(self, row: Dict[str, Any]):
-        if not self._writer:
-            self._writer = csv.DictWriter(self._file, fieldnames=list(row.keys()))
-            self._writer.writeheader()
+    def set_headers(self, headers: List[str]):
+        self._writer = csv.DictWriter(self._file, fieldnames=headers)
+        self._writer.writeheader()
 
-        self._writer.writerow(row)
+    def print_row(self, row: Dict[str, Any]):
+
+        try:
+            self._writer.writerow(row)
+        except AttributeError:
+            # set_headers() was not called before printing a row
+            raise ValueError("CSVPrinter: cannot write a row before the headers having been set")
+
+
